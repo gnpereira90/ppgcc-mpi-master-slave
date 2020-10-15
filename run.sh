8dc80c464f717c51ea6f8f9962c3ba94b3590e26
Carlos Kayser <carloshkayser@gmail.com>
2020-10-15 02:50:35 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,31 +6,35 @@ touch logs/master-slave-sort.csv
 
 TRIES=5
 # CORES=("1" "2" "4" "8" "16")
+ARGS=(10000, 100000, 1000000)
 DIR=$PWD
 OUTPUTFILE_MASTER_SLAVE="$PWD/logs/sort-sequential.csv"
 OUTPUTFILE_SEQUENTIAL="$PWD/logs/master-slave-sort.csv"
 
-number_vectors=10
-array_size=10
+number_vectors=1000
+# array_size=100000
 
 cd "$DIR/sort-sequential"
 make
 echo "TRIES;ARRAY_SIZE;NUMBER_VECTORS;RUNTIME" > "$OUTPUTFILE_SEQUENTIAL"
-for i in $(seq 1 $TRIES); do
-    row=$(make run ARRAY_SIZE=$array_size NUMBER_VECTORS=$number_vectors | grep -i 'RUNTIME' | cut -d "=" -f2)
-    echo "$i;$row" >> "$OUTPUTFILE_SEQUENTIAL"
-    echo "$i;$array_size;$number_vectors;$row"
+for k in $(seq 1 $TRIES); do
+    for ((i = 0; i < ${#ARGS[@]}; i++)); do
+        row=$(make run ARRAY_SIZE=${ARGS[$i]} NUMBER_VECTORS=$number_vectors | grep -i 'RUNTIME' | cut -d "=" -f2)
+        echo "$k; ${#ARGS[@]}; $number_vectors; $row" >> "$OUTPUTFILE_SEQUENTIAL"
+        echo "$k; ${#ARGS[@]}; $number_vectors; $row"
+    done
 done
 
 cd "$DIR/master-slave-sort"
 make
 echo "TRIES;NPS;ARRAY_SIZE;NUMBER_VECTORS;RUNTIME" > "$OUTPUTFILE_MASTER_SLAVE"
-for i in $(seq 1 $TRIES); do
-
+for k in $(seq 1 $TRIES); do
     processes=($(seq 2 2 32))
-
     for np in "${processes[@]}"; do
-        row=$(make run NP=$np ARRAY_SIZE=$array_size NUMBER_VECTORS=$number_vectors | grep -i 'RUNTIME' | cut -d "=" -f2)
-        echo "$i;$np;$array_size;$number_vectors;$row" >> "$OUTPUTFILE_MASTER_SLAVE"
+        for ((i = 0; i < ${#ARGS[@]}; i++)); do
+            row=$(make run NP=$np ARRAY_SIZE=${#ARGS[@]} NUMBER_VECTORS=$number_vectors | grep -i 'RUNTIME' | cut -d "=" -f2)
+            echo "$k; $np; ${#ARGS[@]}; $number_vectors; $row" >> "$OUTPUTFILE_MASTER_SLAVE"
+            echo "$k; $np; ${#ARGS[@]}; $number_vectors; $row"
+        done
     done
 done

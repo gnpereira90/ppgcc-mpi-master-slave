480c40be786994b8ab04ae8390fbf52edf5708ea
Carlos Kayser <carloshkayser@gmail.com>
2020-10-14 23:58:19 -0300

    Script to run the programs

--- run.sh
+++ run.sh
@@ -6,17 +6,17 @@ mkdir -p logs/master-slave-sort
 tries=3
 
 number_vectors=1000
-vector_size=100000
+array_size=100000
 
 cd sort-sequential
 for ((i = 1; i <= $tries; i++)); do
-    make run ARRAY_SIZE=$vector_size NUMBER_VECTORS=$number_vectors > ../logs/sort-sequential/sort-sequential-$i.txt
+    make run ARRAY_SIZE=$array_size NUMBER_VECTORS=$number_vectors > ../logs/sort-sequential/sort-sequential-$i.txt
 done
 
 cd ../master-slave-sort
 for ((i = 1; i <= $tries; i++)); do
     processes=($(seq 2 1 20))
     for np in "${processes[@]}"; do
-        make run NP=$np ARRAY_SIZE=$vector_size NUMBER_VECTORS=$number_vectors > ../logs/master-slave-sort/master-slave-sort-$np-$i.txt
+        make run NP=$np ARRAY_SIZE=$array_size NUMBER_VECTORS=$number_vectors > ../logs/master-slave-sort/master-slave-sort-$np-$i.txt
     done
 done
